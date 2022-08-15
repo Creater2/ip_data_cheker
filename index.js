8a2ea9358db9a0c92d0747f6b5999371561ea2b5
Creater2 <105660444+Creater2@users.noreply.github.com>
2022-08-16 02:15:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function func(){
+
+
+    let apiKey = '1be9a6884abd4c3ea143b59ca317c6b2';
+    $.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
+    
+      
+    console.log(JSON.stringify(data, null, 2,));
+
+    $(".div_1").text(JSON.stringify(data, null, 2,));
+
+
+
+    });
+    
+    }
+    
+    setTimeout(func, 1)
+    
